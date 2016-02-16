04a0afa5903072c811e7900ddb7f027d32a3daf6
nelso <nels.oscar@live.com>
2016-02-16 15:26:31 -0800

    Added routes
    Endpoints for handling new reviews, removed hardcoding the review.

--- routes.py
+++ routes.py
@@ -1,41 +1,69 @@
-from bottle import route, view, request, response
+from bottle import route, view, request, response, redirect
 from datetime import datetime
 
 from bibdb import *
 
 import json, os, bibtexparser, hashlib
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-DOCS_ROOT = os.path.join(PROJECT_ROOT, 'docs').replace('\\', '/')
-
-__temp_review = Review.select().where(Review.name=='Visualization Design Frameworks').get()
+from util import *
 
 @route('/')
 @view('index')
 def prototype():
     return {}
 
+@route('/add_review')
+@view('add_review')
+def prototype():
+    return {"msg": session().get('msg','')}
+
+@route('/handle_review', method='POST')
+def prototype():
+    title = request.forms.title
+    description = request.forms.description
+    if title and description:
+        r = Review(name=title, description=description)
+        r.save(force_insert=True)
+        session()['msg'] = "Success"
+        return redirect('/add_review')
+    session()['msg'] = "Failed, missing argument."
+    return redirect('/add_review')
+
+@route('/curr_review/<name>')
+@view('add_review')
+def prototype(name="Defualt"):
+    r = Review.select().where(Review.name==name).count()
+    if r > 0:
+        session()['curr_review'] = name
+        return redirect('/add')
+    session()['msg'] = 'Invalid review'
+    return redirect('/')
+
+
 @route('/add')
 @view('add')
 def prototype():
-    return {"msg": ""}
+    return {"msg": session().get('msg','')}
 
-@route('/upload', method="POST")
+@route('/handle_document', method="POST")
 def prototype():
     cite = request.forms.cite
     data = request.files.data
     
+    cr = current_review()
+
     if cite and data and data.file:
         # Parse bib
         try:
             bib = bibtexparser.loads(cite)
         except:
-            return {"msg": "bogus bib"}
+            session()['msg'] = "bogus bib"
+            return redirect('/add')
 
         # encode the bib
         json_bib = json.dumps(bib.entries[0])
-        hashed_name = hashlib.md5(json_bib).hexdigest()
+        # hash the bib with the review; save separate copies per review.
+        hashed_name = hashlib.md5(json_bib+cr.name).hexdigest()
         hashed_filename = hashed_name + '.pdf'
 
         # write the pdf to file.
@@ -44,13 +72,16 @@ def prototype():
 
         # Redirect the user to the add page
         response.status = 303
-        response.set_header('Location', '/add)
+        response.set_header('Location', '/add')
 
         # Write the db entry for the document.
-        doc = Document(md5=hashed_name, name=bib.entries[0]['title'], bib=json_bib, notes="", review=__temp_review)
-        doc.save()
-        return {"msg": "Success"}
-    return {"msg": "You missed a field."}
+        doc = Document(md5=hashed_name, name=bib.entries[0]['title'], bib=cite.strip(' \t\n\r'), notes="", review=cr)
+        doc.save(force_insert=True)
+        session()['msg'] = "Success"
+        return redirect('/add')
+
+    session()['msg'] = "You missed a field."
+    return redirect('/add')
 
 @route('/annotate')
 @view('list_documents')
@@ -61,3 +92,9 @@ def prototype():
 @view('annotate_document')
 def prototype(md5=None):
     return {"doc": md5}
+
+@route('/paper/<md5>')
+@view('annotate_document')
+def prototype(md5=None):
+    from bottle import static_file
+    return static_file(md5+'.pdf', root=DOCS_ROOT)
