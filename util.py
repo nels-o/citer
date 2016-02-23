cfc2e36ed39e2cc5a756c690bf214c262833e249
nelso <nels.oscar@live.com>
2016-02-23 11:02:23 -0800

    General utilities for templates.

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 from bottle import request
 from bibdb import *
-import os
+import os, hashlib
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
@@ -11,3 +11,43 @@ def session():
 
 def current_review():
     return Review.select().where(Review.name==session().get('curr_review','Default')).get()
+
+def get_binary_file(filepath):
+	with open(filepath, 'rb') as f:
+		contents = f.read()
+	md5 = hashlib.md5()
+	md5.update(contents)
+	return (contents, md5)
+
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+
+def block_code(text, lang, inlinestyles=False, linenos=False):
+    if not lang:
+        text = text.strip()
+        return u'<pre><code>%s</code></pre>\n' % mistune.escape(text)
+
+    try:
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter(
+            noclasses=inlinestyles, linenos=linenos
+        )
+        code = highlight(text, lexer, formatter)
+        if linenos:
+            return '<div class="highlight-wrapper">%s</div>\n' % code
+        return code
+    except:
+        return '<pre class="%s"><code>%s</code></pre>\n' % (
+            lang, mistune.escape(text)
+        )
+
+
+class HighlightMixin(object):
+    def block_code(self, text, lang):
+        # renderer has an options
+        inlinestyles = self.options.get('inlinestyles')
+        linenos = self.options.get('linenos')
+        return block_code(text, lang, inlinestyles, linenos)
