24d34aeeb8a453fe759c1a8e4ee24432736a1de4
nelso <nels.oscar@live.com>
2016-02-28 15:33:31 -0800

    Add dictionary merge function

--- util.py
+++ util.py
@@ -18,3 +18,13 @@ def get_binary_file(filepath):
 	md5 = hashlib.md5()
 	md5.update(contents)
 	return (contents, md5)
+
+def merge_dicts(*dict_args):
+    '''
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    '''
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
