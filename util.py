bf46b96ad8277b44d2966b1129eacffe3cc62fbd
nelso <nels.oscar@live.com>
2016-02-28 15:11:16 -0800

    Removing pygments
    Experimental bad idea.

--- util.py
+++ util.py
@@ -18,36 +18,3 @@ def get_binary_file(filepath):
 	md5 = hashlib.md5()
 	md5.update(contents)
 	return (contents, md5)
-
-
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters import HtmlFormatter
-
-
-def block_code(text, lang, inlinestyles=False, linenos=False):
-    if not lang:
-        text = text.strip()
-        return u'<pre><code>%s</code></pre>\n' % mistune.escape(text)
-
-    try:
-        lexer = get_lexer_by_name(lang, stripall=True)
-        formatter = HtmlFormatter(
-            noclasses=inlinestyles, linenos=linenos
-        )
-        code = highlight(text, lexer, formatter)
-        if linenos:
-            return '<div class="highlight-wrapper">%s</div>\n' % code
-        return code
-    except:
-        return '<pre class="%s"><code>%s</code></pre>\n' % (
-            lang, mistune.escape(text)
-        )
-
-
-class HighlightMixin(object):
-    def block_code(self, text, lang):
-        # renderer has an options
-        inlinestyles = self.options.get('inlinestyles')
-        linenos = self.options.get('linenos')
-        return block_code(text, lang, inlinestyles, linenos)
